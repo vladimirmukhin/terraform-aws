f15e5264d3c5ca8fa4df219f0abefbf9c035a06f
Vladimir <awsvlad@gmail.com>
2022-07-27 13:29:13 -0400

    Configure remote state

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-remote-state-abc123abc"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-remote-state"
+  }
 }
 
 provider "aws" {
