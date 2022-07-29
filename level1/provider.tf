ce4b533bc2fde06c2344ef291b87903e53e160d0
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2022-07-29 11:53:13 -0400

    Configure terraform layers (#10)

--- level1/provider.tf
+++ level1/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "terraform-remote-state-abc123abc"
-    key            = "terraform.tfstate"
+    key            = "level1.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-remote-state"
   }
---
+++ level2/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-remote-state-abc123abc"
+    key            = "level2.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-remote-state"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
