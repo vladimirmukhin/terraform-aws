fdef503c3839357494b01617d9a10c597c5ffb95
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2022-08-15 23:04:19 -0400

    Add IAM role (#13)

--- level2/asg.tf
+++ level2/asg.tf
@@ -5,6 +5,7 @@ resource "aws_launch_configuration" "main" {
   security_groups = [aws_security_group.private.id]
   user_data       = file("user-data.sh")
   key_name        = "main"
+  iam_instance_profile = aws_iam_instance_profile.main.name
 }
 
 resource "aws_autoscaling_group" "main" {
