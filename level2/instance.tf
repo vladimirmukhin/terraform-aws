415e59abdea6645a37455d5acf361f4c0b444c5a
Vladimir <awsvlad@gmail.com>
2022-08-18 19:40:15 -0400

    Add ssm

--- level2/instance.tf
+++ level2/instance.tf
@@ -14,64 +14,10 @@ data "aws_ami" "amazonlinux" {
   owners = ["137112412989"]
 }
 
-resource "aws_instance" "public" {
-  ami                         = data.aws_ami.amazonlinux.id
-  associate_public_ip_address = true
-  instance_type               = "t2.micro"
-  key_name                    = "main"
-  vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[1]
-
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
-resource "aws_security_group" "public" {
-  name        = "${var.env_code}-public"
-  description = "Allow inbound traffic"
-  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    description = "SSH from public"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["98.180.121.31/32"]
-  }
-
-  ingress {
-    description = "HTTP from public"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["98.180.121.31/32"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
 resource "aws_security_group" "private" {
   name        = "${var.env_code}-private"
   description = "Allow VPC traffic"
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
-  }
 
   ingress {
     description = "HTTP from load balancer"
