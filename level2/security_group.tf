e4d521d941bd30a3d1e61fbbf975e2be0118f67d
Vladimir <awsvlad@gmail.com>
2022-08-19 21:53:29 -0400

    Reorder level2

--- level2/security_group.tf
+++ level2/security_group.tf
@@ -1,19 +1,3 @@
-data "aws_ami" "amazonlinux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["137112412989"]
-}
-
 resource "aws_security_group" "private" {
   name        = "${var.env_code}-private"
   description = "Allow VPC traffic"
