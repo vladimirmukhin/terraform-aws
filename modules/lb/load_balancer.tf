d58c89d6cd785491e0d16adeaa33d013e7ad10c8
Vladimir <awsvlad@gmail.com>
2022-08-22 21:29:49 -0400

    Change load balancer port

--- modules/lb/load_balancer.tf
+++ modules/lb/load_balancer.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "load_balancer" {
 
   ingress {
     description = "http to ELB"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
